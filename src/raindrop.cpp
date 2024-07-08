27fb7e4a090a64e88f9f256bc6fe18969d15cff1
fele-systems <61920953+fele-systems@users.noreply.github.com>
2024-07-07 21:56:15 -0300

    fix: Move json array to vector instead of copying

--- src/raindrop.cpp
+++ src/raindrop.cpp
@@ -104,7 +104,7 @@ bool get_raindrops(const RaindropAccount &radindropio, uint64_t id, std::vector<
     auto fetched_records = perpage * page + items.size();
     auto total_records = j["count"].get<int>();
 
-    result = items;
+    result = std::move(items);
 
-    return !items.empty() && fetched_records < total_records;
+    return !result.empty() && fetched_records < total_records;
 }
